1cb11542a0335bf6787e8c670563c5ddb0efa90e
Bohdan Holubnychyi <bohdanholubn@gmail.com>
2023-07-25 00:42:07 +0300

    Update main.py

--- tomato_time/main.py
+++ tomato_time/main.py
@@ -14,7 +14,6 @@ reps = 0
 timer = None
 
 # TIMER RESET
-
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text='00:00')
@@ -22,8 +21,8 @@ def reset_timer():
     check_mark.config(text='')
     global reps
     reps = 0
-# TIMER MECHANISM
 
+# TIMER MECHANISM
 def start_timer():
     global reps
     reps += 1
@@ -67,7 +66,6 @@ window = Tk()
 window.title('Pomodorooooooo')
 window.config(padx=100, pady=50, bg=YELLOW)
 
-
 start_button = Button(text='START', fg='blue', bg='white', highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
@@ -83,11 +81,7 @@ canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
 
-
-
 check_mark = Label(fg=GREEN, bg=YELLOW, highlightthickness=0)
 check_mark.grid(column=1, row=3)
-
-
 
 window.mainloop()
