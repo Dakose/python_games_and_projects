14a10a8d22e5212a1fce79f1971ce6cbba933ffb
Bohdan Holubnychyi <bohdanholubn@gmail.com>
2023-07-25 00:04:08 +0300

    added timer mechanism
    added timer mechanism

--- tomato_time/main.py
+++ tomato_time/main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import math
 
 # CONSTANTS
 PINK = "#e2979c"
@@ -14,16 +15,39 @@ LONG_BREAK_MIN = 20
 
 # TIMER MECHANISM
 
-# COUNTDOWN MECHANISM
+def start_timer():
+    global reps
+    reps += 1
 
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN
+
+
+    count_down(work_sec)
+    count_down(long_break_sec)
+    count_down(short_break_sec)
+# COUNTDOWN MECHANISM
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    if count_sec <= 10:
+        count_sec = f'0{count_sec}'
+
+    canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
+    if count > 0:
+        window.after(1000, count_down, count - 1)
 
 # UI SETUP
 window = Tk()
 window.title('Pomodorooooooo')
 window.config(padx=100, pady=50, bg=YELLOW)
 
-start_button = Button(text='START', fg='blue', bg='white', highlightthickness=0)
+
+start_button = Button(text='START', fg='blue', bg='white', highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
+
 reset_button = Button(text='RESET', fg='blue', bg='white', highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
@@ -33,8 +57,10 @@ label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file='tomato.png')
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+timer_text = canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
 canvas.grid(column=1, row=1)
+
+
 
 check_mark = Label(text='✔', fg=GREEN, bg=YELLOW, highlightthickness=0)
 check_mark.grid(column=1, row=3)
