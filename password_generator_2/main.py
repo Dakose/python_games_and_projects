4d7e6522b20178c47bd53062431b19008eda1287
Bohdan Holubnychyi <bohdanholubn@gmail.com>
2023-08-19 18:07:46 +0300

    search button
    Added search button and search func for searching login and passwords to you websites in  JSON file

--- password_generator_2/main.py
+++ password_generator_2/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
+
 
 # PASSWORD GENERATOR
 def generate_password():
@@ -32,17 +34,49 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            'email': email,
+            'password': password,
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title='Ooops', message='Please make sure you haven`t left any fields empty')
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f'These are the details entered: \nEmail: {email} '
-                                                      f'\nPassword: {password} \nIs it OK to save?')
-        if is_ok:
-            with open('data.txt', 'a') as data_file:
-                data_file.write(f'{website} | {email} | {password}\n')
-                website_entry.delete(0, END)
-                password_entry.delete(0, END)
+        try:
+            with open('data.json', 'r') as data_file:
+                # reading old data
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open('data.json', 'w') as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            # Updating old data with new data
+            data.update(new_data)
+
+            with open('data.json', 'w') as data_file:
+                # Saving update data
+                json.dump(data, data_file, indent=4)
+        finally:
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+#  SEARCH PASSWORD
+def find_password():
+    website = website_entry.get()
+    try:
+        with open('data.json') as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message='No Data File Found.')
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title=website, message=f'Email: {email}\nPassword: {password}')
+        else:
+            messagebox.showinfo(title='Error', message=f'No details for {website} exists.')
 
 # UI SETUP
 
@@ -64,20 +98,21 @@ password_label = Label(text='Password')
 password_label.grid(row=3, column=0)
 
 # entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=40)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, '')
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 # buttons
-generate_button = Button(text='Generate Password', command=generate_password)
+generate_button = Button(text='Generate Password', command=generate_password, width=15)
 generate_button.grid(row=3, column=2)
 add_button = Button(text='Add', width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
-
+search_button = Button(text='Search', width=15, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
